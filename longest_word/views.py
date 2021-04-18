32dede800ecf92093d05e199dad2d67405b99316
Edward Bent <bentorama@gmail.com>
2021-04-18 17:48:53 +0100

    Added logic for generating letters and styling

--- longest_word/views.py
+++ longest_word/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import string, random
 
 # Create your views here.
 def new(request):
-  return HttpResponse("Hello, world!")
+  alphabet_string = string.ascii_uppercase
+  alphabet_list = list(alphabet_string)
+  letters_list = random.sample(alphabet_list, 10)
+  context = {
+    'letters': letters_list
+  }
+  return render(request, "longest_word/new.html", context)
